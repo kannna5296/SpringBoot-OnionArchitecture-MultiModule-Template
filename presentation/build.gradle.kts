c5e54bd50ea61ae2aad97d9981f7380a83f89337
kannna5296 <kannnahacks5296@gmail.com>
2022-08-28 16:27:17 +0900

    デシリアライズモジュール追加

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -4,5 +4,5 @@ dependencies {
     // DI対象(domain層のRepositoryインターフェース)指定のためruntimeで入れる
     // 参考 > https://blog.takehata-engineer.com/entry/realizing-an-onion-architecture-in-kotlin-and-spring-boot-with-gradle-multi-project
     runtimeOnly(project(":infra"))
-    // implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin") //Form用のデシリアライズに用いる
 }
