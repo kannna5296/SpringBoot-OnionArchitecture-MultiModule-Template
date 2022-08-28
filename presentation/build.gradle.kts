33441eb0d9c361bf460cff1908a94b655c5602ec
kannna5296 <kannnahacks5296@gmail.com>
2022-08-28 17:15:29 +0900

    コメント

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -4,7 +4,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin") // Form用のデシリアライズに用いる
     implementation(project(":usecase"))
     implementation(project(":domain"))
-    // preseintation->infraの依存は必要。これがないとpresentation->usecase->infraというDIの流れが実現できずbootRun失敗する
+    // 本来CleanArchitecture的にはpreseintation->infraの依存は不要。
+    // ただDIの都合上、依存させないとpresentation->usecase->infraというDIの流れが実現できずbootRun失敗する
     // 一方で、presentation層にinfra層のクラスを直接importさせたくない。そのため[runtime]で入れる
     // 参考 > https://blog.takehata-engineer.com/entry/realizing-an-onion-architecture-in-kotlin-and-spring-boot-with-gradle-multi-project
     runtimeOnly(project(":infra"))
