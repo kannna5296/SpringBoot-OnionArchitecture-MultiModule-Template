aa2053aa3e987588028ed92218c62abc8759840d
kannna5296 <kannnahacks5296@gmail.com>
2022-08-28 17:03:56 +0900

    依存を各モジュールに分散

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,12 +31,8 @@ allprojects {
 
 subprojects {
     dependencies {
-        implementation("org.springframework.boot:spring-boot-starter")
-        implementation("org.springframework.boot:spring-boot-starter-web")
-        implementation("org.springframework.boot:spring-boot-starter-data-jpa")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-        runtimeOnly("com.h2database:h2")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
     }
 }
--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,8 +1,11 @@
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin") // Form用のデシリアライズに用いる
     // usecaseに依存
     implementation(project(":usecase"))
     // DI対象(domain層のRepositoryインターフェース)指定のためruntimeで入れる
     // 参考 > https://blog.takehata-engineer.com/entry/realizing-an-onion-architecture-in-kotlin-and-spring-boot-with-gradle-multi-project
     runtimeOnly(project(":infra"))
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin") // Form用のデシリアライズに用いる
+
 }
