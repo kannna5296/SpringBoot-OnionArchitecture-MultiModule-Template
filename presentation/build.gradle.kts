23c58067bf169a7ff42ca2bea69ead4d4aabf17c
kannna5296 <kannnahacks5296@gmail.com>
2022-08-28 17:10:42 +0900

    presentationを他の全モジュールに依存するよう修正

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -4,6 +4,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin") // Form用のデシリアライズに用いる
     // usecaseに依存
     implementation(project(":usecase"))
+    implementation(project(":domain"))
     // DI対象(domain層のRepositoryインターフェース)指定のためruntimeで入れる
     // 参考 > https://blog.takehata-engineer.com/entry/realizing-an-onion-architecture-in-kotlin-and-spring-boot-with-gradle-multi-project
     runtimeOnly(project(":infra"))
