6429966684b5d0782f0a5eed5c9a006b52c9f7fb
kannna5296 <kannnahacks5296@gmail.com>
2022-08-28 17:14:19 +0900

    コメント

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -2,10 +2,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin") // Form用のデシリアライズに用いる
-    // usecaseに依存
     implementation(project(":usecase"))
     implementation(project(":domain"))
-    // DI対象(domain層のRepositoryインターフェース)指定のためruntimeで入れる
+    // preseintation->infraの依存は必要。これがないとpresentation->usecase->infraというDIの流れが実現できずbootRun失敗する
+    // 一方で、presentation層にinfra層のクラスを直接importさせたくない。そのため[runtime]で入れる
     // 参考 > https://blog.takehata-engineer.com/entry/realizing-an-onion-architecture-in-kotlin-and-spring-boot-with-gradle-multi-project
     runtimeOnly(project(":infra"))
 
