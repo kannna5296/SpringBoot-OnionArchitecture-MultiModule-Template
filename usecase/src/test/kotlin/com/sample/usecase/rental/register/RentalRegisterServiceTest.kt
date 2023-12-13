7a1e6134c0cafa94306691cf99277de1bc67e4a8
kannna5296 <kannnahacks5296@gmail.com>
2023-12-13 22:07:16 +0900

    テストファイル

---
+++ usecase/src/test/kotlin/com/sample/usecase/rental/register/RentalRegisterServiceTest.kt
@@ -0,0 +1,14 @@
+package com.sample.usecase.rental.register
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
+class RentalRegisterServiceTest{
+
+    @Test
+    fun execute() {
+        assertEquals(1, 1)
+    }
+
+}
