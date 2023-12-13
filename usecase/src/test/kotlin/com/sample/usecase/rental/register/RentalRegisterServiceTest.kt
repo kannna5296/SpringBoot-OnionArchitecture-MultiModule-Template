9f18588309d72521834ccc01f11568db9ee45276
kannna5296 <kannnahacks5296@gmail.com>
2023-12-13 23:17:28 +0900

    テスト一件追加

--- usecase/src/test/kotlin/com/sample/usecase/rental/register/RentalRegisterServiceTest.kt
+++ usecase/src/test/kotlin/com/sample/usecase/rental/register/RentalRegisterServiceTest.kt
@@ -1,14 +1,49 @@
 package com.sample.usecase.rental.register
 
+import com.sample.domain.book.IBookRepository
+import com.sample.domain.rental.IRentalRepository
+import com.sample.domain.user.IUserRepository
+import com.sample.domain.user.Mail
+import com.sample.domain.user.Name
+import com.sample.domain.user.Phone
+import com.sample.domain.user.RentalUser
+import io.mockk.every
+import io.mockk.mockk
+import java.time.ZonedDateTime
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
+class RentalRegisterServiceTest {
 
-class RentalRegisterServiceTest{
+    private val userRepository = mockk<IUserRepository>()
+    private val bookRepository = mockk<IBookRepository>()
+    private val rentalRepository = mockk<IRentalRepository>()
+
+    private val target = RentalRegisterService(
+        userRepository = userRepository,
+        bookRepository = bookRepository,
+        rentalRepository = rentalRepository,
+    )
 
     @Test
-    fun execute() {
-        assertEquals(1, 1)
+    fun `書籍が存在しないとエラー`() {
+        every { userRepository.findById(any()) } returns RentalUser(
+            id = 1,
+            name = Name("name"),
+            phone = Phone("phone"),
+            mail = Mail("mail"),
+        )
+        every { bookRepository.findById(any()) } returns null
+        val ex = assertFailsWith<IllegalArgumentException> {
+            target.execute(
+                RentalRegisterForm(
+                    userId = 1,
+                    bookId = 1,
+                    deadline = ZonedDateTime.now(),
+                )
+            )
+        }
+        assertEquals("book-not-found", ex.message)
     }
-
 }
