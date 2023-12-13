680f0fadb233937d42c92eaf4f954e033389c69a
kannna5296 <kannnahacks5296@gmail.com>
2023-12-13 22:14:11 +0900

    テスト用gradle追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,8 @@ subprojects {
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+        testImplementation(kotlin("test"))
     }
 }
 
