30e69badf3a2ba2f834492a880f45818794ba084
kannna5296 <kannnahacks5296@gmail.com>
2023-12-23 19:04:36 +0900

    format

--- domain/src/test/kotlin/com/sample/domain/user/MailTest.kt
+++ domain/src/test/kotlin/com/sample/domain/user/MailTest.kt
@@ -4,20 +4,19 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 
-internal class MailTest{
+internal class MailTest {
 
     @Test
-    fun `インスタンス化可能`(){
+    fun `インスタンス化可能`() {
         val input = "hoge@example.com"
         val mail = Mail(input)
         assertEquals(input, mail.value)
     }
 
     @Test
-    fun `不正な文字列なのでインスタンス化できない`(){
+    fun `不正な文字列なのでインスタンス化できない`() {
         val input = "hogeexample.com"
-        val ex = assertFailsWith<IllegalArgumentException>{Mail(input)}
+        val ex = assertFailsWith<IllegalArgumentException> { Mail(input) }
         assertEquals(" invalid-mail-address", ex.message)
     }
-
 }
