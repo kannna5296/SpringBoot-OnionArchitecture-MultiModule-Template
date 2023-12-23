50207e2192538840403a5402a01d4b12ffbcdb87
kannna5296 <kannnahacks5296@gmail.com>
2023-12-23 19:04:11 +0900

    ドメインテスト追加

--- domain/src/main/kotlin/com/sample/domain/user/Mail.kt
+++ domain/src/main/kotlin/com/sample/domain/user/Mail.kt
@@ -1,4 +1,9 @@
 package com.sample.domain.user
 
 // ValueObject例
-data class Mail(val value: String)
+data class Mail(val value: String){
+
+    init {
+        require(value.matches(Regex("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}"))) {" invalid-mail-address"}
+    }
+}
