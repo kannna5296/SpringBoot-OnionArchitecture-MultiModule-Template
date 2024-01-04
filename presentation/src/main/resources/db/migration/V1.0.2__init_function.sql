0b1f0456dd9fe9769a9bf555907c63959806b090
yamane <kannnahacks5296@gmail.com>
2024-01-04 22:38:04 +0900

    ファンクション練習追加

--- presentation/src/main/resources/db/migration/V1.0.2__init_function.sql
+++ presentation/src/main/resources/db/migration/V1.0.2__init_function.sql
@@ -1,13 +1,25 @@
-CREATE FUNCTION get_rental_user
+CREATE OR ALTER FUNCTION get_rental_user
 (
     @name NVARCHAR(50),
     @phone NVARCHAR(50)
 )
-RETURNS TABLE
-AS
-RETURN
+RETURNS @UserInfo TABLE
 (
-    SELECT *
+    user_id INT,
+    user_name VARCHAR(100)
+)
+AS
+BEGIN
+    DECLARE @user_id INT
+    DECLARE @user_name VARCHAR(100)
+
+    SELECT
+        @user_id = id,
+        @user_name = name
     FROM rental_user
     WHERE phone = @phone OR name = @name
-)
+
+    INSERT INTO @UserInfo (user_id, user_name)
+    VALUES (@user_id, @user_name)
+    RETURN
+END
