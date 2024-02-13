1d264eeb1b24cab514815407a0fb01dedc2c2b45
yamane <kannnahacks5296@gmail.com>
2024-02-13 15:17:23 +0900

    自前でID指定しないよう修正

--- presentation/src/main/resources/data.sql
+++ presentation/src/main/resources/data.sql
@@ -1,17 +1,21 @@
-INSERT INTO rental_user VALUES(1, 'tom', '000-0000', 'example1@gmail.com', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-INSERT INTO rental_user VALUES(2, 'jerry', '000-0001', 'example2@gmail.com', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO rental_user (name, phone, mail, created_at, updated_at)
+  VALUES
+('tom', '000-0000', 'example1@gmail.com', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+('jerry', '000-0001', 'example2@gmail.com', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
-INSERT INTO book VALUES(1, 'hoge1', 'author1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-INSERT INTO book VALUES(2, 'hoge2', 'author2', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-INSERT INTO book VALUES(3, 'hoge3', 'author3', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-INSERT INTO book VALUES(4, 'hoge4', 'author4', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
-INSERT INTO book VALUES(5, 'hoge5', 'author5', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO book (name, author, created_at, updated_at)
+  VALUES
+('hoge1', 'author1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+('hoge2', 'author2', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+('hoge3', 'author3', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+('hoge4', 'author4', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP()),
+('hoge5', 'author5', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
--- 返却ずみ
-INSERT INTO rental VALUES(1, 1, 1, '2024-01-08', 1, '2024-01-01', '2024-01-01');
--- 未返却
-INSERT INTO rental VALUES(2, 2, 1, '2024-01-09', 0, '2024-01-02', '2024-01-02');
-INSERT INTO rental VALUES(3, 3, 1, '2024-01-10', 0, '2024-01-03', '2024-01-03');
---INSERT INTO rental VALUES(4, 4, 2, DATEADD('DAY', 1, CURRENT_DATE), 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
---INSERT INTO rental VALUES(5, 5, 2, DATEADD('DAY', 1, CURRENT_DATE), 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+INSERT INTO rental (book_id, user_id, deadline, is_returned, created_at, updated_at)
+ VALUES
+(1, 1, '2024-01-08', 1, '2024-01-01', '2024-01-01'),-- 返却ずみ
+(2, 1, '2024-01-09', 0, '2024-01-02', '2024-01-02'),-- 未返却
+(3, 1, '2024-01-10', 0, '2024-01-03', '2024-01-03');-- 未返却
+--(4, 2, DATEADD('DAY', 1, CURRENT_DATE), 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
+--(5, 2, DATEADD('DAY', 1, CURRENT_DATE), 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP());
 
